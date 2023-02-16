c70aa4416a22902fb37c723f2305d3e0f093d293
27JuneWMF2Rutvik <bhambhirutvik2004@gmail.com>
2023-02-16 17:54:55 +0530

    react

--- src/CommonComponent/Component/FunctionalComponent/FunctionalCompoMenu.jsx
+++ src/CommonComponent/Component/FunctionalComponent/FunctionalCompoMenu.jsx
@@ -5,11 +5,15 @@ class ClassCompoMenu extends Component {
     return (
       <>
         <div className="row">
-          <div className="col-6 offset-6">
+          <div className="col-6 offset-6"> 
             <ul>
-              <li>
-                <Link to="functionalclock">Clock</Link>
-              </li>
+              <li><Link to="functionalclock">Clock</Link></li>
+              <li><Link to="functionalcompoexample">Intro</Link></li>
+              <li><Link to="stateexampleinfunctionalcompo">State</Link></li>
+              <li><Link to="useeffectexampleinfunctionalcompo">useEffect</Link></li>
+              <li><Link to="uselayouteffectexampleinfunctionalcompo">useLayoutEffect</Link></li>
+              <li><Link to="usecontextinfunctionalcompo">useContext</Link></li>
+              <li><Link to="usereducerinfunctionalcompo">usereducer</Link></li>
             </ul>
           </div>
         </div>
